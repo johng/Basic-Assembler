87fbeef948387ab4c9268d867a23d41a8876f925
John Griffith <john.griffith@live.co.uk>
2015-02-18 00:44:09 +0000

    Converts assembly instructions into hexadecimal opcode to the file out.txt.
    If a line begins with ';' it is ignored as a comment.

--- assemmbler.c
+++ assemmbler.c
@@ -1,11 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char* readline(char * line){
+int binaryToInt(char * input){
+	char * ptr;
+	ptr = input;
 	
-	char *output;
-	output = (char *)malloc(8);
+	int out = 0;
+	int mult = 1;
+	while(*ptr!='\0'){
+		
+		if(*ptr == '1'){
+			out += mult;
+		}
 
+		mult *= 2;
+		++ptr;
+
+
+	}
+	return out;
+
+}
+
+char *strrev(char * str){
+
+	char * p1, *p2;
+	if(! str || ! *str){
+		return str;
+	}
+
+	for(p1 = str,p2 =str+strlen(str) -1;p2 > p1; ++p1, --p2){
+		*p1 ^= *p2;
+		*p2 ^= *p1;
+		*p1 ^= *p2;
+	}
+
+	return str;
+
+}
+
+char* readline(FILE *out, char * line){
+	
+	
+
+	int b0 = 0;
+	int b1 = 0;
 	if(*line == ';' ){
 
 		return NULL;
@@ -13,64 +53,104 @@ char* readline(char * line){
 	
 	if(*line == ':'){
 		printf("change");
+	
 	}
 
 	//If not ':' or ';' split string into tokens
 
-	char *token = strtok(line," ");
+	char *token;
+	char s[2] = " ";
+	size_t len = sizeof(line);
+	char local[len+1];
+	strcpy(local,line);
+	token = strtok(local, s);	
+	
+	
+	if(!strcmp(token,"INC")){
+		
+		b1 = 0;
+		b0 = strtok(NULL," ")[1] - '0';
+	
+	}else if(!strcmp(token,"DEC")){
+	
+		b1 = 0;
+		b0 = 2 + strtok(NULL, " ")[1] - '0';
 
-	if(token == "INC"){
+	}else if(!strcmp(token,"JNZ")){
+		b0 = 4;
+		
+		token = strtok (NULL, " ");
+		if(*token == ':'){
+		}else{
+			b1 = strtol(token, NULL, 10);
+		}
+		
 
+	}else if(!strcmp(token,"JNEG")){
+	
+		b0 = 6;
+		token = strtok(NULL, " ");
+		
+		if(*token == ':'){
 
-	}else if(token == "DEC"){
+		}else{
+			b1 = strtol(token, NULL, 10);
+		}
 
-	}else if(token == "JNZ"){
+	}else if(!strcmp(token,"STR")){
+		
+		b0 = 8 + *strtok(NULL, " ") - '0';
+		token = strtok(NULL," ");
+		if(*token == ':'){
+		}else{
+			b1 = strtol(token, NULL,10);
+		}
+	}else if(!strcmp(token,"LDR")){
+		
+		b0 = 10 + *strtok(NULL, " ") - '0';
 
-
-	}else if(token == "JNEG"){
-
-
-	}else if(token == "STR"){
-
-	}else if(token == "LDR"){
-
-
+		token = strtok(NULL, " ");
+		
+		if(*token == ':'){
+		}else{
+			b1 = strtol(token, NULL, 10);
+		}
+	
 	}
 
-	
+	fprintf(out,"%x%x\n",b0,b1);
 
 	return line;
 }
 
 int main(int argc, char *argv[]){
-	
+
 	if(argc<1){
 		printf("No arguments!\n");
 		return 1;
 	}
+	
 	// Reading the assembly file
 	FILE *ptr_file;
 	FILE *out_file;	
 
-	ptr_file = fopen(*argv[1],"r");
+	ptr_file = fopen(argv[1],"r");
 	
 	out_file = fopen("out.txt","w");
 	
 	if(!ptr_file || !out_file){
+		printf("Exit bad files");
 		return 1;
 	}
 
 	char buff[4096];
-	while( fgets(buff,sizeof buff,ptr_file) != NULL){	
-		
-		fprintf(out_file, readline(buff));
-		
 	
+	while( fgets(buff,sizeof buff,ptr_file) != NULL){		
+		readline(out_file,buff);
 	}
-
+	
 	fclose(ptr_file);
 	fclose(out_file);
 	
 	return -1;
-
 }
