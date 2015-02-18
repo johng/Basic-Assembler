6ae88c8ef6f9081134d5ad925ded03bb6031d37d
John Griffith <john.griffith@live.co.uk>
2015-02-18 02:10:05 +0000

    Supported labels to hold addresses.
    Opcodes can support those labels in their instructions.

--- assemmbler.c
+++ assemmbler.c
@@ -2,6 +2,16 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+
+typedef struct label {
+	int address;
+	char * name;
+	struct label * next;
+}label;
+
+void insertLabel(label ** insert, char * line, int n);
+
 int binaryToInt(char * input){
 	char * ptr;
 	ptr = input;
@@ -36,23 +46,98 @@ char *strrev(char * str){
 		*p1 ^= *p2;
 	}
 
-	return str;
-
+}
+/*
+	Debug function to print the contents of a label linked list
+	to the terminal
+*/
+void printLabelList(label * root){
+	if(root != NULL){
+		printf("Address: %d Name: %s\n",root->address,root->name);
+		printLabelList(root->next);
+	}
 }
 
-char* readline(FILE *out, char * line){
+/*
+	Generates a linked list of labels
+*/
+void labelGenerator(char * line, int * n,label ** root){
+	
+	if(*line != ':'){
+		if(*line != ';'){
+			*n=*n+1;
+		}
+		return;
+	}
+
+
+	char * ptr;
+	ptr = line;
+	ptr++;
+
+	insertLabel(root,ptr,*n);
+}
+
+/*
+	Inserts entry into linked list of labels.
+	If label is already present in the linked list the 
+	insert operation will stop. 
+*/
+void insertLabel(label ** insert, char * line, int n){
+	
+	if(*insert == NULL){
+	
+		*insert  = (label *) malloc (sizeof(label));
+		
+		(*insert)->name = (char*)malloc(sizeof(line));
+		strcpy((*insert)->name,line);
+		(*insert)->address = n;
+		(*insert)->next = NULL;
+		
+	}else{
+		
+		if( !strcmp((*insert)->name, line)){
+			return;
+		}else {
+			insertLabel(&(*insert)->next,line,n);
+		}
+	}
+}
+
+/*
+	Finds the address of a label from a given name
+	If no address is found, return -1;
+*/
+int getAddress(char * name,label * list){
+	char *ptr;
+	ptr = name;
+	if(*ptr == ':'){
+		ptr++;
+	}
+
+	if(list!=NULL){
+		if(!strcmp(list->name,ptr)){
+			return list->address;
+		}else{
+			return getAddress(ptr,list->next);
+		}
+	}
+	//End condition if nothing found at end of the list
+	return -1;
+}
+
+void readline(FILE *out, char * line,label * addressList){
 	
 	
 
 	int b0 = 0;
 	int b1 = 0;
-	if(*line == ';' ){
-
-		return NULL;
+	if(*line == ';'){
+		return ;
 	}
 	
 	if(*line == ':'){
-		printf("change");
+		return ;
 	
 	}
 
@@ -81,6 +166,7 @@ char* readline(FILE *out, char * line){
 		
 		token = strtok (NULL, " ");
 		if(*token == ':'){
+			b1 = getAddress(token,addressList);
 		}else{
 			b1 = strtol(token, NULL, 10);
 		}
@@ -92,7 +178,7 @@ char* readline(FILE *out, char * line){
 		token = strtok(NULL, " ");
 		
 		if(*token == ':'){
-
+			b1 = getAddress(token,addressList);
 		}else{
 			b1 = strtol(token, NULL, 10);
 		}
@@ -102,6 +188,7 @@ char* readline(FILE *out, char * line){
 		b0 = 8 + *strtok(NULL, " ") - '0';
 		token = strtok(NULL," ");
 		if(*token == ':'){
+			b1 = getAddress(token,addressList);
 		}else{
 			b1 = strtol(token, NULL,10);
 		}
@@ -112,6 +199,7 @@ char* readline(FILE *out, char * line){
 		token = strtok(NULL, " ");
 		
 		if(*token == ':'){
+			b1 = getAddress(token,addressList);
 		}else{
 			b1 = strtol(token, NULL, 10);
 		}
@@ -120,7 +208,7 @@ char* readline(FILE *out, char * line){
 
 	fprintf(out,"%x%x\n",b0,b1);
 
-	return line;
+	return ;
 }
 
 int main(int argc, char *argv[]){
@@ -132,7 +220,10 @@ int main(int argc, char *argv[]){
 	
 	// Reading the assembly file
 	FILE *ptr_file;
-	FILE *out_file;	
+	FILE *out_file;
+
+	label * root;
+	root = NULL;
 
 	ptr_file = fopen(argv[1],"r");
 	
@@ -144,10 +235,20 @@ int main(int argc, char *argv[]){
 	}
 
 	char buff[4096];
-	
-	while( fgets(buff,sizeof buff,ptr_file) != NULL){		
-		readline(out_file,buff);
+	int n = 0;	
+	while (fgets(buff,sizeof buff,ptr_file)!=NULL){
+		labelGenerator(buff,&n,&root);
 	}
+
+	//Start at the top of file, ready for re reading;
+	fseek(ptr_file,0,SEEK_SET);
+	
+	printLabelList(root);
+
+	while( fgets(buff,sizeof buff,ptr_file) != NULL){		
+		readline(out_file,buff,root);
+	}
+
 	
 	fclose(ptr_file);
 	fclose(out_file);
